63cd20790b754ad3e86e09ecc031cd358daa8d34
metricmachine <122141904+metricmachine@users.noreply.github.com>
2023-01-20 15:48:18 -0800

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1199},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about-us","displayName":"About Us","link":{"linkType":"LinkTypePage","href":"#!page-about-us"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-mobile-home","displayName":"Mobile-Home","link":{"linkType":"LinkTypePage","href":"#!page-mobile-home"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
